5547bffba93c8861a28447e89faf7480ba03e7b7
sembb <sembarelds37@gmail.com>
2020-12-08 12:20:12 +0100

    first commit

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text class='pliptekst' onPress = {plip}>{this.state.myText}</Text>
       <StatusBar style="auto" />
     </View>
   );
+  
 }
 
 const styles = StyleSheet.create({
@@ -19,3 +21,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+function plip(){
+  document.getElementsByClassName('pliptekst').setState({pliptekst: 'plip'})
+}
+
+
